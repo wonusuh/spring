b9976ba833fc1e8a1c9e31c5facf0be1f412ab9a
wonusuh <wonwooseo23@gmail.com>
2020-04-08 17:41:24 +0900

    add BoardMapperTests

--- src/main/java/kr/or/yonsai/mapper/BoardMapper.java
+++ src/main/java/kr/or/yonsai/mapper/BoardMapper.java
@@ -3,10 +3,19 @@
  */
 package kr.or.yonsai.mapper;
 
+import java.util.List;
+
+import org.apache.ibatis.annotations.Select;
+
+import kr.or.yonsai.domain.BoardVO;
+
 /**
  * @author YONSAI
  *
  */
 public interface BoardMapper {
+
+	@Select("SELECT * FROM tbl_board WHERE bno > 0")
+	public List<BoardVO> getList();
 
 }
