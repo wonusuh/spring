f324f9027181b32378f74813d953a030b251164c
wonusuh <wonwooseo23@gmail.com>
2020-04-08 17:42:50 +0900

    modified more

---
+++ src/test/java/kr/or/yonsai/mapper/BoardMapperTests.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package kr.or.yonsai.mapper;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/**
+ * @author YONSAI
+ *
+ */
+public class BoardMapperTests {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
